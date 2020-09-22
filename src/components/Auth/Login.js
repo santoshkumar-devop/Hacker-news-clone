b623288e32f89fc70ad5562c8eea8e318c1e3e9f
santosh kumar <santoshkumar.devop@gmail.com>
2020-09-22 20:43:43 +0530

    Added Reusable logic and Validation Logic

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,7 +1,77 @@
 import React from "react";
+import useFormValidation from "./useFormValidation";
+import validateLogin from "./validateLogin";
+
+const INITIAL_STATE = {
+  name: "",
+  email: "",
+  password: "",
+};
 
 function Login(props) {
-  return <div>Login</div>;
+  const {
+    handleSubmit,
+    handleChange,
+    values,
+    handleBlur,
+    errors,
+    isSubmitting,
+  } = useFormValidation(INITIAL_STATE, validateLogin);
+  const [login, setLogin] = React.useState();
+
+  return (
+    <div>
+      <h2 className="mv3">{login ? "Login" : "Create Account"}</h2>
+      <form onSubmit={handleSubmit} className="form">
+        <input
+          type="text"
+          name="name"
+          value={values.name}
+          placeholder="your name"
+          onChange={handleChange}
+          autoComplete="off"
+        />
+        <input
+          type="email"
+          name="email"
+          onBlur={handleBlur}
+          values={values.name}
+          onChange={handleChange}
+          className={errors.email && "error-input"}
+          placeholder="Your email"
+          autoComplete="off"
+        />
+        {errors.email && <p className="error-text">{errors.email}</p>}
+        <input
+          type="password"
+          values={values.password}
+          onBlur={handleBlur}
+          onChange={handleChange}
+          className={errors.password && "error-input"}
+          name="password"
+          placeholder="Choose a secure password"
+        />
+        {errors.password && <p className="error-text">{errors.password}</p>}
+        <div className="flex mt3">
+          <button
+            type="submit"
+            className="button pointer mr2"
+            disabled={isSubmitting}
+            style={{ background: isSubmitting ? "grey" : "orange" }}
+          >
+            Submit
+          </button>
+          <button
+            type="button"
+            className="pointer button"
+            onClick={() => setLogin((prevLogin) => !prevLogin)}
+          >
+            {login ? "need to create an acount" : "already have an account?"}
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,37 @@
 import React from "react";
+import { NavLink, withRouter } from "react-router-dom";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <div className="navbar">
+        <img src="/logo.png" alt="Hooks News Logo" className="logo" />
+        <NavLink to="/" className="header-title">
+          Hooks News
+        </NavLink>
+        <NavLink to="/" className="header-link">
+          new
+        </NavLink>
+        <div className="divider">|</div>
+        <NavLink to="/top" className="header-link">
+          top
+        </NavLink>
+        <div className="divider">|</div>
+        <NavLink to="/search" className="header-link">
+          Search
+        </NavLink>
+        <div className="divider">|</div>
+        <NavLink to="/submit" className="header-link">
+          Submit
+        </NavLink>
+      </div>
+      <div className="navbar">
+        <NavLink to="/login" className="header-link">
+          Login
+        </NavLink>
+      </div>
+    </div>
+  );
 }
 
-export default Header;
+export default withRouter(Header);
